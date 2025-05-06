81725599b8d594f09219bb4ab7c0dcbd2b55a44c
jsampsontrinitas <jjdsampson+trinitas@gmail.com>
2025-05-06 21:23:39 +0000

    Refactor Projectile class to improve initialization and update logic; add projectile management in index.js

--- oo/classes/Projectile.js
+++ oo/classes/Projectile.js
@@ -7,47 +7,121 @@ export default class Projectile {
   speed;
   maxRange;
   maxDamage;
+
+  /**
+   * @type {HTMLDivElement}
+   * @description The projectile's HTML element. This is the
+   * element that will be drawn to the screen.
+   */
   element;
 
   constructor(options) {
-    this.x = options.x;
-    this.y = options.y;
-    this.angle = options.angle;
+    const defaults = {
+      angle: 0,
+      x: 0,
+      y: 0,
+      startX: 0,
+      startY: 0,
+      speed: 2,
+      maxRange: 300,
+      maxDamage: 10,
+    };
 
+    let opts = { ...defaults, ...options };
+
+    this.angle = opts.angle;
+    this.x = opts.x;
+    this.y = opts.y;
+    this.startX = opts.startX || opts.x;
+    this.startY = opts.startY || opts.y;
+    this.speed = opts.speed;
+    this.maxRange = opts.maxRange;
+    this.maxDamage = opts.maxDamage;
+
+    this.createAndAttachElement();
+  }
+
+  /**
+   * @description Creates a new HTML element for the projectile
+   * and attaches it to the document.
+   */
+  createAndAttachElement() {
     this.element = document.createElement("div");
-    this.element.style.width = "5px";
-    this.element.style.height = "5px";
+    this.element.style.width = "3px";
+    this.element.style.height = "3px";
     this.element.style.borderRadius = "50%";
     this.element.style.backgroundColor = "black";
     this.element.style.position = "absolute";
-
-    this.draw();
+    this.element.style.left = this.x + "px";
+    this.element.style.top = this.y + "px";
 
     document.body.appendChild(this.element);
   }
 
-  move() {
-    const newX = this.x + Math.cos(this.angle) * this.speed;
-    const newY = this.y + Math.sin(this.angle) * this.speed;
+  /**
+   * @returns {void} With no `return` statement, we implicitly return void.
+   * @description Updates the projectile's position based on its speed
+   * and angle. If the next position will be invalid (e.g., out of view),
+   * then the project is destroyed. If the next position is valid, it
+   * will be applied to the projectile's internal state.
+   */
+  update() {
+    const nextX = this.x + Math.cos(this.angle) * this.speed;
+    const nextY = this.y + Math.sin(this.angle) * this.speed;
 
-    const xIsValid = newX > 0 && newX < innerWidth;
-    const yIsValid = newY > 0 && newY < innerHeight;
+    const xIsValid = nextX > 0 && nextX < innerWidth;
+    const yIsValid = nextY > 0 && nextY < innerHeight;
 
-    if (xIsValid && yIsValid) {
-      this.x = newX;
-      this.y = newY;
-    } else {
+    if (!xIsValid || !yIsValid) {
       this.destroy();
+      return;
     }
+
+    this.x = nextX;
+    this.y = nextY;
   }
 
+  /**
+   * @description Draws the projectile at its current position. This
+   * is done by setting the `top` and `left` CSS properties of the
+   * projectile's element to the projectile's current `x` and `y`
+   * coordinates.
+   */
   draw() {
     this.element.style.top = this.y + "px";
     this.element.style.left = this.x + "px";
   }
 
+  /**
+   * @returns {boolean} Returns true if the projectile's element is
+   * still connected to the document. This is used to determine if
+   * the projectile should be drawn to the screen or not.
+   */
+  isConnected() {
+    return this.element.isConnected;
+  }
+
+  /**
+   * @description Destroys the projectile by removing it from the
+   * DOM. It also calculates the distance traveled by the projectile
+   * and logs it to the console.
+   */
   destroy() {
+    /**
+     * Detaches the element from the document/page. We check to see if
+     * a projectile is attached or not when we decide which projectiles
+     * to continue drawing.
+     */
     this.element.remove();
-    console.log("Projectile has been destroyed");
+
+    /**
+     * Calculate the straight-line (Euclidean) distance from the
+     * starting point to the current position.
+     */
+    const distX = this.x - this.startX;
+    const distY = this.y - this.startY;
+    const distTraveled = Math.round(Math.hypot(distX, distY));
+
+    console.log(`Projectile destroyed after traveling ${distTraveled}px`);
   }
 }
--- oo/index.js
+++ oo/index.js
@@ -5,11 +5,30 @@
 import Projectile from "./classes/Projectile.js";
 import Tank from "./classes/Tank.js";
 
+/**
+ * We'll track all active projectiles in an array. This will make it easier for
+ * us to cycle over them later, updating their locations, checking for collisions,
+ * and more. We'll use `let` to create this array, since we'll be overwriting it
+ * from time to time (as we add and remove projectiles).
+ *
+ * Below is what's referred to as a "JSDoc" comment. It provides additional info
+ * about the variable. This states that our array is an array of Projectile
+ * objects. Plus, when you hover over `projectiles` anywhere in this file, you'll
+ * see a more helpful tooltip appear. Try it out, and compare with what you see
+ * when you hover over `armyTank` or `pinkTank` below.
+ * @type { Projectile[] }
+ */
+let projectiles = [];
+
 // We're using `const` to create variables `armyTank` and `pinkTank`. Because
 // we used `const` (and not `let`), we won't be able to assign a new value to
 // either of theve variables without causing an error. This protects us from
 // accidentally overwriting variables.
-const armyTank = new Tank({ rounds: 5, color: "green" });
+const tank = new Tank({
+  location: { x: 100, y: 100 },
+  rounds: 5,
+  color: "green",
+});
 
 // While the `pinkTank` variable is unused at this point (unlike the `armyTank`
 // cariable which was used to call `armyTank.render()`), it serves as a good
@@ -31,7 +50,7 @@ const pinkTank = new Tank({ color: "pink" });
 // added by the Machine.js file, as part of the Machine class. Because Tank is
 // a type of Automobile, which itself is a type of Machine, Tank therefore
 // "inherits" the render method.
-armyTank.render();
+tank.render();
 
 const pressedKeys = {
   KeyW: false,
@@ -44,37 +63,100 @@ const pressedKeys = {
   ArrowDown: false,
 };
 
+function updateProjectiles() {
+  /**
+   * We're going to overwrite the `projectiles` variable with a filtered version
+   * of itself. The filter cycles over all projectiles in the array, updates their
+   * position (which might result in the projectile being "destroyed"), and then
+   * chooses whether to keep the projectile in the array or not. If the projectile
+   * is still connected to the DOM, we keep it. If not, we remove it from the array.
+   */
+  projectiles = projectiles.filter((projectile) => {
+    /**
+     * Inside this method, whether the projectile is kept in our array or not is
+     * based on the return value. If we ultimately return `true`, the projectile
+     * will remain in the array. If we return `false`, it will be removed.
+     */
+    projectile.update();
+
+    /**
+     * If the projectile's element is still connected to the DOM, we will draw it
+     * at its new location, then return `true` to keep it around a bit longer.
+     */
+    if (projectile.isConnected()) {
+      projectile.draw();
+      return true;
+    }
+
+    /**
+     * If the `return true` above didn't get executed, then `false` will be our
+     * default return value. This means the projectile will be removed from the
+     * array, and no longer drawn to the screen moving forward.
+     */
+    return false;
+  });
+}
+
 function update() {
+  // Loop through all projectiles, updating their state
+  updateProjectiles();
+
   // Is the user driving forward/backward?
   if (pressedKeys.KeyW || pressedKeys.ArrowUp) {
-    armyTank.driveForward();
+    tank.driveForward();
   } else if (pressedKeys.KeyS || pressedKeys.ArrowDown) {
-    armyTank.driveBackward();
+    tank.driveBackward();
   }
 
   // Is the user turning?
   if (pressedKeys.KeyA || pressedKeys.ArrowLeft) {
-    armyTank.turnLeft();
+    tank.turnLeft();
   } else if (pressedKeys.KeyD || pressedKeys.ArrowRight) {
-    armyTank.turnRight();
+    tank.turnRight();
   }
 
-  armyTank.update();
+  tank.update();
 
   requestAnimationFrame(update);
 }
 
 update();
 
+/**
+ * @description Creates a new projectile, passing a reference to
+ * one of our tank so that we can use the x/y/angle properties.
+ * @param {Tank} tank
+ */
+function createProjectile(tank) {
+  /**
+   * We start the projectile at the location of our tank (the
+   * placement is a little off because x/y is not the center of
+   * our tank, but rather our rear left corner). We also give
+   * the projectile the same angle as the tank, so it will fly
+   * in the same direction as the tank is facing.
+   */
+  const projectile = new Projectile({
+    x: tank.location.x,
+    y: tank.location.y,
+    angle: tank.angle,
+  });
+
+  /**
+   * Lastly, we push this new projectile object into to the array
+   * of projectiles.
+   */
+  projectiles.push(projectile);
+}
+
 document.addEventListener("keydown", (event) => {
+  /**
+   * If the user pressed the spacebar, we want to create a new
+   * projectile. We'll call the `createProjectile` function, passing
+   * in the tank object. We pass in a reference to our tank so that
+   * we can apply the tank's location and angle to the projectile.
+   */
   if (event.code == "Space") {
-    // armyTank.shoot();
-    new Projectile({
-      x: Math.random() * innerWidth,
-      y: Math.random() * innerHeight,
-      move: 1,
-      angle: Math.random() * Math.PI * 2,
-    });
+    createProjectile(tank);
   }
 
   if (event.code in pressedKeys) {
